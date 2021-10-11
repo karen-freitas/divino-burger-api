bee91ab536879136dca8d8e3e853dd25ff242f4b
Karen Freitas <karenfcorrea@gmail.com>
2021-10-11 12:18:15 -0300

    teste deploy mudança database-url production

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -1,14 +1,15 @@
 'use strict';
+
 const {Model} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class users extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
     }
   };
   users.init({
@@ -18,12 +19,30 @@ module.exports = (sequelize, DataTypes) => {
     role: DataTypes.STRING,
     restaurant: DataTypes.STRING
   }, {
-    sequelize,
+      sequelize,
     modelName: 'users',
   });
   return users;
 };
 
+// const {DataTypes} =require("sequelize")
+// const sequelize = require("../sequelize");
+
+// const users = sequelize.define("user",{
+//     name: DataTypes.STRING,
+//     email: DataTypes.STRING,
+//     password: DataTypes.STRING,
+//     role: DataTypes.STRING,
+//     restaurant: DataTypes.STRING
+
+// });
+// const init = async () => {
+//   await users.sync();
+// };
+
+// init();
+
+// module.exports = users;
 // module.exports = (sequelize, Sequelize) => {
 //   const users = sequelize.define( "users", {
 //     name: {
