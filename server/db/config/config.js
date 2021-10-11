f3bb268e589404298a3faa5448a536b1f5bd5d9d
Karen Freitas <karenfcorrea@gmail.com>
2021-10-11 12:32:04 -0300

    erro deploy

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -1,26 +1,26 @@
 
 require('dotenv').config();
 module.exports = {
-  "development": {
-    "use_env_variable": process.env.DATABASE_URL,
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": {
-        " require": true,
-        "rejectUnauthorized": false
-      }
-    }
-  },
-
   // "development": {
-  //   "username": "postgres",
-  //   "password": "luigipixel17",
-  //   "database": "divino-burger-api",
-  //   "host": "127.0.0.1",
+  //   "use_env_variable": process.env.DATABASE_URL,
   //   "dialect": "postgres",
-  //   "port": 5432,
-
+  //   "dialectOptions": {
+  //     "ssl": {
+  //       " require": true,
+  //       "rejectUnauthorized": false
+  //     }
+  //   }
   // },
+
+  "development": {
+    "username": "postgres",
+    "password": "luigipixel17",
+    "database": "divino-burger-api",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "port": 5432,
+
+  },
   "test": {
     "username": "root",
     "password": null,
