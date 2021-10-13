912c7a1343c12ae26d965b6c34201362f6483ef6
Karen Freitas <karenfcorrea@gmail.com>
2021-10-13 09:42:51 -0300

    configurações .env

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -1,6 +1,10 @@
 
-require('dotenv/config')
+require('dotenv').config()
+// require('dotenv').config({path:'../../../server/.env'});
 console.log(process.env.DATABASE_USER)
+console.log(process.env.DATABASE_HOST)
+
+
 
 module.exports = {
   // "development": {
@@ -23,25 +27,11 @@ module.exports = {
     
   // },
 
-  //  "development": {
-  //   "username": process.env.DATABASE_USER,
-  //   "password": process.env.DATABASE_PASSWORD,
-  //   "database": process.env.DATABASE_DATABASE,
-  //   "host": process.env.HOST,
-  //   "dialect": "postgres",
-  //   "dialectOptions": {
-  //     "ssl": {
-  //       "require": true,
-  //       "rejectUnauthorized": false
-  //     }
-  //   }
-  // },
-
-  "development": {
-    "username": "qyparswnfqliom",
-    "password": "316d28fb16cbabff8da69496b2b1e38cc76828a7d5a53b45939c198628b5f27b",
-    "database": "daern93cekmj3r",
-    "host": "ec2-44-199-86-61.compute-1.amazonaws.com",
+   "development": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_DATABASE,
+    "host": process.env.DATABASE_HOST,
     "dialect": "postgres",
     "dialectOptions": {
       "ssl": {
@@ -50,6 +40,20 @@ module.exports = {
       }
     }
   },
+
+  // "development": {
+  //   "username": "qyparswnfqliom",
+  //   "password": "316d28fb16cbabff8da69496b2b1e38cc76828a7d5a53b45939c198628b5f27b",
+  //   "database": "daern93cekmj3r",
+  //   "host": "ec2-44-199-86-61.compute-1.amazonaws.com",
+  //   "dialect": "postgres",
+  //   "dialectOptions": {
+  //     "ssl": {
+  //       "require": true,
+  //       "rejectUnauthorized": false
+  //     }
+  //   }
+  // },
 
   "test": {
     "username": "root",
