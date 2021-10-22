d331d55467ddfb943c5b1078cfd637f98c74b3b6
Karen Freitas <karenfcorrea@gmail.com>
2021-10-22 11:47:33 -0300

    documentação de usuários

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/* eslint-disable space-before-blocks */
-/* eslint-disable global-require */
-if (process.env.NODE_ENV !== 'production'){ require('dotenv').config(); }
+// eslint-disable-next-line global-require
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config(); }
 
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
 const routes = require('./server/routes/index');
 
 const app = express();
@@ -14,9 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get('*', (req, res) => {
-  res.send('Hello World!');
+  res.send('API Restaurante Divino Burger.');
 });
 
 app.listen(port, () => {
